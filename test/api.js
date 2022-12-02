b431c64375c6afca84138cf9a5c547e491184e2e
Ilya Koshelev <mifomen@yandex.ru>
2022-12-02 09:05:29 +0300

    test

---
+++ test/api.js
@@ -0,0 +1,114 @@
+// import { points } from './data.js';
+import { getData, renderTextQuestion, shuffleArray, renderAnswerArray } from './utils.js';
+
+
+const template = /^\D\D\s\D\D\s?\d\D/;
+
+const name = 'Горемыка Иван 11А';
+console.log(`template.test(name)=${template.test(name)}`)
+
+let randomLineQuests = []; // = shuffleArray(DATA);
+const btnStartTest = document.querySelector('.btn-start-test');
+
+
+const countQuestionBar = (countItems) => {
+  const ul = document.createElement('ul');
+  ul.className = 'progress-bar';
+
+  if (document.querySelector('.js-init-game')) {
+    document.querySelector('.js-init-game').appendChild(ul);
+  }
+
+  for (let j = 1; j <= countItems.length; j++) {
+    const button = document.createElement('button');
+    button.className = 'progress-bar__item';
+
+    button.textContent = j;
+    button.onselectstart = 'return false';
+    button.onmousedown = 'return false'
+    button.disabled = 'true';
+    ul.appendChild(button);
+  }
+};
+
+let DATA_GET_URL = './questions1.json';
+
+const allBtns = document.querySelectorAll('.js-btn-start');
+for (const allBtn of allBtns) {
+  allBtn.addEventListener('click', (evt) => {
+    DATA_GET_URL = allBtn.dataset.nameTest;
+    startGame();
+  })
+}
+
+const startGame = () => {
+  document.querySelector('.frame-init-game').classList.add('hidden');
+  if (document.querySelector('.tester-info').value =='') {
+    localStorage.userName = document.querySelector('.tester-info').placeholder
+  } else {
+    localStorage.userName = document.querySelector('.tester-info').value;
+  }
+
+
+  getData((questionArray) => {
+    randomLineQuests = shuffleArray(questionArray);
+    countQuestionBar(randomLineQuests);
+    document.querySelector('.js-init-game').classList.remove('hidden');
+    document.querySelector('.progress-bar__item').classList.add('progress-bar__item--active');
+    const fremeInitGame = document.querySelector('.js-init-game');
+    renderTextQuestion(fremeInitGame, randomLineQuests[0]);
+    // const divUl = document.querySelector('.progress-bar'); //render 1 question
+    renderAnswerArray(randomLineQuests, fremeInitGame, randomLineQuests[0].arrayAnswers);
+
+  });
+
+};
+
+document.querySelector('body').onselectstart = 'return false';
+document.querySelector('body').onmousedown = 'return false';
+
+const btnStartDisabled = (state) => {
+  const btns = document.querySelectorAll('.js-btn-start');
+  for (const btn of btns) {
+    btn.disabled = state;
+  }
+};
+
+const valueToArray = function (item) {
+  return item.value.split(' ');
+};
+
+const templateData = /[A-Za-zА-Яа-яЁё]/;
+
+const testName = (item) => {
+  if (templateData.test(item)) {
+    return true;
+  }
+};
+
+const checkEveryName = (item) => {
+  if (item.length > 3) {
+    return (valueToArray(item).every(testName));
+  }
+};
+
+const inputTesterName = document.querySelector('.tester-info');
+inputTesterName.focus();
+inputTesterName.addEventListener('input', () => {
+  const valueLength = inputTesterName.value.length;
+
+  if (valueToArray(inputTesterName).length >= 2 && valueToArray(inputTesterName)[1].length >= 2) {
+    inputTesterName.value = inputTesterName.value.trim();
+    btnStartDisabled(false);
+    // document.querySelector('.js-btn-start').disabled = false;
+  } else {
+    btnStartDisabled(true);
+    // document.querySelector('.js-btn-start').disabled = true;
+    inputTesterName.setCustomValidity('Должно быть только фамилия и имя, 2 слова');
+  }
+
+  inputTesterName.reportValidity();
+});
+
+
+export { DATA_GET_URL };
---
+++ test/utils.js
@@ -0,0 +1,156 @@
+import {DATA_GET_URL} from './api.js';
+
+const findItem = function (items, resolve) {
+  return items.find(item => item.id.toString() === resolve);
+};
+
+const findItemByText = function (items, resolve) {
+  return items.find(item => item.qustionText.toString() === resolve);
+};
+
+const getRandomInt = (min, max) => {
+  if (min >= max) {
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+const clearQuestionArea = () => {
+  if (document.querySelector('.area-choose-answer')) {
+    document.querySelector('.area-choose-answer').remove();
+  }
+  if (document.querySelector('.question-text')) {
+    document.querySelector('.question-text').remove();
+  }
+}
+
+const renderTextQuestion = (parent, data) => {
+  const span = document.createElement('span');
+  span.className = 'question-text';
+  span.innerHTML = data.qustionText;
+  span.onselectstart = 'return false';
+  span.onmousedown = 'return false';
+  parent.appendChild(span);
+}
+
+const shuffleArray = (basicArray) => {
+  let newArr = new Array();
+  for (let elementBasicArray of basicArray) {
+    newArr.push(elementBasicArray);
+  }
+  return newArr.sort(() => Math.random() - 0.5);
+  // return newArr;
+};
+
+////  установить значение для ключа
+//// localStorage.test = 2;
+//// // получить значение по ключу
+//// alert( localStorage.test ); // 2
+//// удалить ключ
+//// delete localStorage.test;
+
+let i = 1;
+const choosenAnswers = [];
+const renderAnswerArray = (data, parent, item) => {
+  if (document.querySelector('.area-choose-answer')) {
+    document.querySelector('.area-choose-answer').remove();
+  }
+
+  const div = document.createElement('div');
+  div.classList.add('area-choose-answer');
+  if (item.length <= 4) {
+    div.classList.add('grid-2x2');
+  } else {
+    div.classList.add('grid-3x3');
+  }
+
+  for (const answer of shuffleArray(item)) {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'answer';
+    button.innerHTML = answer;
+    button.onselectstart = 'return false';
+    button.onmousedown = 'return false';
+
+    const allProgressItems = document.querySelectorAll('.progress-bar__item');
+    button.onclick = (evt) => {
+      document.querySelector('.progress-bar__item--active').disabled = 'true';
+      choosenAnswers.push(evt.target.textContent);
+      clearQuestionArea();
+      // console.log(`i=${i} allProgressItems.length=${allProgressItems.length} allProgressItems=${allProgressItems[0]}`);
+      // allProgressItems[i].classList.add('progress-bar__item--active');
+      if (i < allProgressItems.length) {
+        allProgressItems[i].classList.add('progress-bar__item--active');
+        let obj = findItemByText(data, data[i].qustionText);
+        renderTextQuestion(document.querySelector('.js-init-game'), obj);
+        renderAnswerArray(data, document.querySelector('.js-init-game'), obj.arrayAnswers);
+      }
+      i++;
+      if ( allProgressItems.length + 1 == i ) {
+        showAnswers(data, choosenAnswers);
+      }
+    }
+
+    div.appendChild(button);
+  }
+  parent.appendChild(div);
+
+};
+
+const showLocal = () => {
+  for (let key in localStorage) {
+    console.log(`key=${key}`);
+  }
+};
+
+
+
+
+const getData = (onSuccess) => {
+  fetch(DATA_GET_URL)
+    .then((response) => response.json())
+    .then((questionsArray) => {
+      onSuccess(questionsArray);
+    });
+};
+
+let pointsTesting = 0;
+const showAnswers = (arrayRightAnswer, arrayGetAnswer) => {
+  document.querySelector('.js-init-game').classList.add('hidden');
+  const div = document.createElement('div');
+  div.className = 'frame-resault-game';
+  div.onselectstart = 'return false';
+  div.onmousedown = 'return false';
+
+  for (let i = 0; i < arrayGetAnswer.length; i++) {
+    const spanElement = document.createElement('span');
+    spanElement.classList.add('resaultAnswer');
+    spanElement.onselectstart = 'return false';
+    spanElement.onmousedown = 'return false';
+
+    if (choosenAnswers[i] === arrayRightAnswer[i].arrayAnswers[0]) {
+      spanElement.classList.add('rightResaultAnswer');
+      spanElement.innerHTML = `В вопросе №${i + 1}. Ваш ответ правильный:   <span class="corrent-answer">${arrayRightAnswer[i].arrayAnswers[0]}</span>`;
+      pointsTesting++
+    } else {
+      spanElement.classList.add('loseResaultAnswer');
+      spanElement.innerHTML = `В вопросе №${i + 1}. Ваш ответ &nbsp <span class="red">неправильный</span>:&nbsp<span class="not-corrent-answer">${arrayGetAnswer[i]}</span>`;
+    }
+    div.appendChild(spanElement);
+  }
+  // console.log(`pointsTesting=${pointsTesting}`)
+  localStorage.pointsTesting = pointsTesting;
+  localStorage.pointsLength = arrayGetAnswer.length;
+
+  const spanElement = document.createElement('span');
+  spanElement.classList.add('resaultAnswer');
+  spanElement.onselectstart = 'return false';
+  spanElement.onmousedown = 'return false';
+  spanElement.innerHTML=`${localStorage.userName} получил:<br>${pointsTesting} баллов из ${choosenAnswers.length}`;
+  div.appendChild(spanElement);
+  document.body.appendChild(div);
+};
+
+
+export { getData, showLocal, findItem, clearQuestionArea, shuffleArray, renderTextQuestion, getRandomInt, renderAnswerArray };
